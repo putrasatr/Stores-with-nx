dbee9c1bb29e52a4cc922d0d7971bcb42bc9fc0c
putrasatr <satriaputrapratama2002@gmail.com>
2022-05-31 16:45:58 +0700

    gallery login

--- apps/gallery/src/Login.svelte
+++ apps/gallery/src/Login.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    
+  import './app.css';
   export let name: string;
   export const onSubmit = () => {
     console.log('Submit');
@@ -24,25 +24,3 @@
     <button type="submit">Submit</button>
   </form>
 </main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
